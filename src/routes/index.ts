5c95a02e11bd38b34a09ac34c0ea889cffdf99b8
João <povoajoao6@gmail.com>
2022-07-03 16:45:59 -0300

    Mustache {{}} - Conditionals, loopings | Without MVC Concept yet

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,10 +3,29 @@ import { Router, Response, Request } from "express";
 const router = Router();
 
 router.get('/', (req:Request, res:Response)=> {
+    let age: number = 20
+    let renderContent: boolean = false
+
+    if(age > 50) {
+        renderContent = true
+    } 
+
     res.render('home', {
         name: 'waheed',
         lastname: 'sakir',
-        age: 20
+        renderContent,
+        sentenceOfTheDay: [
+            "Pensei na noite Sonhei com a lua Quem eu entrei Foi o Exu Tranca Rua.",
+            "Era reluzente e muito elegante E quando gargalhava Tinha um poder deslumbrante Poder da lua.",
+            "Rei da encruzilhada Mora no cemitério A sua gargalhada Possui um mistério.",
+            "Pois é Tranca Rua Exu dono da Lua Salve, Salve, Salve Glorioso Tranca Rua",
+        ],
+        shoppingList: [
+            {product: 'MAÇA', price: 2.00},
+            {product: 'BANANA', price: 1.99},
+            {product: 'BERGAMOTA', price: 0.8},
+            {product: 'MAMÃO', price: 34.6}
+        ]
     });
 });
 
